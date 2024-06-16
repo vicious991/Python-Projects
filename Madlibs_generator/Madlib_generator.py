767b8d2e5be83270579c23313968a790ec42ac47
0RBIT <166555285+vicious991@users.noreply.github.com>
2024-06-16 12:05:24 +0530

    Update Madlib_generator.py

--- Madlibs_generator/Madlib_generator.py
+++ Madlibs_generator/Madlib_generator.py
@@ -1,1 +1,37 @@
+#This Python script is designed to read a text file, identify specific words enclosed in angle brackets < >,
+#  prompt the user to provide replacement words for each identified word, and then output the modified text 
+# with the replacements.
 
+
+with open ("story.txt","r") as f: #opening the story.txt as f # it should be in same folder
+
+    story = f.read() 
+words = set()
+start_of_word = -1
+
+target_start = "<"
+target_end = ">"
+
+#Identify words enclosed in angle brackets and store them in the words set.
+for i ,char in enumerate(story):
+    if char == target_start:
+        start_of_word= i
+
+    if char == target_end and start_of_word != -1:
+        word = story[start_of_word: i + 1]
+        words.add(word)
+        start_of_word = -1
+
+#Prompt the user to enter replacement words for each identified word and store them in the answers dictionary.
+answers = {}
+
+for word in words:
+    answer  = input("Enter A Word For"+ word + ": ")
+    answers[word]= answer
+
+#Replace the identified words in the story with the user-provided replacements.
+for word in words:
+    story = story.replace(word,answers[word])
+
+# Print the modified story with the replacements.
+print(story)
